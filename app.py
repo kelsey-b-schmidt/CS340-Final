92355066bc8a33b784842a9d9868e087f00378de
schmkelsosu <schmkels@oregonstate.edu>
2022-07-20 23:30:25 -0500

    testing app4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ cors = CORS(app)
 @app.route("/api", methods = ["GET"])
 @cross_origin()
 def index():
-    return {"tutorial": "Flask React Heroku"}
+    return {"tutorial": "Kelsey and Andy's CS340 Project"}
 
 @app.route("/")
 @cross_origin()
