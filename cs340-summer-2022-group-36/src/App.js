1bc6a9a202258cdef305ab4e6a372a97b881d27b
schmkelsosu <schmkels@oregonstate.edu>
2022-07-21 20:16:05 -0500

    testing mySQL

--- cs340-summer-2022-group-36/src/App.js
+++ cs340-summer-2022-group-36/src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import Deploy from './Components/Deploy'
+//import Deploy from './Components/Deploy'
 
 export default function App() {
 
 	const [data, setData] = useState({})
+	const [test, setTest] = useState("Test")
 
 	useEffect(() => {
         const getResponse = async() => {
             const response = await fetch("/api")
             const responseJson = await response.json()
-			setData(responseJson["tutorial"])
+			//setData(responseJson["tutorial"])
+			setData("data")
+			setTest("testing")
         }
         getResponse()
 			.catch(console.error)
@@ -22,6 +25,8 @@ export default function App() {
 	return (
 		<div className="App">
 			<h1>Hello?</h1>
+			<h2>{data}</h2>
+			<h2>{test}</h2>
 		</div>
 	)
 }
