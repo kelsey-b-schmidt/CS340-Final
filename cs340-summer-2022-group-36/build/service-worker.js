aaeb6c00fa8a6c972e9562f49855765db534f865
schmkelsosu <schmkels@oregonstate.edu>
2022-07-26 18:15:39 -0500

    testing pages

--- cs340-summer-2022-group-36/build/service-worker.js
+++ cs340-summer-2022-group-36/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2cbe0a1ccad54cd8bef97ea823fdc68.js"
+  "/precache-manifest.08b3e14ee2d21ddf7ae828968ead00a0.js"
 );
 
 workbox.clientsClaim();
