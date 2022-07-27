a619a1b4d977436748805a13344ddbfb869cd431
schmkelsosu <schmkels@oregonstate.edu>
2022-07-26 19:49:34 -0500

    testing pages

---
+++ cs340-summer-2022-group-36/build/precache-manifest.4dbb3c47e948bf88801471dda68d2b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1893a4624c99282961e1",
+    "url": "/static/css/main.02347a90.chunk.css"
+  },
+  {
+    "revision": "1893a4624c99282961e1",
+    "url": "/static/js/main.1893a462.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "e9f55e3b87d71bfa57f5",
+    "url": "/static/js/2.e9f55e3b.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "c656a542aa4079cbc31d9f20558f6c5c",
+    "url": "/index.html"
+  }
+];
