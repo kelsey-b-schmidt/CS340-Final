984c21b85fb1e1ebb84c520c4f990b9fef04b35d
Kelsey Schmidt <schmkels@oregonstate.edu>
2022-07-28 23:45:38 -0500

    delete

---
+++ cs340-summer-2022-group-36/build/precache-manifest.5288d5a9dce956f6b86b9345690b89ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6617270ca66e720cced9",
+    "url": "/static/css/main.9da670ea.chunk.css"
+  },
+  {
+    "revision": "6617270ca66e720cced9",
+    "url": "/static/js/main.4f945e60.chunk.js"
+  },
+  {
+    "revision": "454ad46b82c0a070bb3c",
+    "url": "/static/js/runtime~main.4c66592b.js"
+  },
+  {
+    "revision": "59e9d0ba9a237170708f",
+    "url": "/static/js/2.bcf7b3ee.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "cd9dbf08226537b05ab21cc3b5478997",
+    "url": "/index.html"
+  }
+];
